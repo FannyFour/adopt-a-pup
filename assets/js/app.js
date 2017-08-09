dd0a685ae330bc98d63cdc2b398fd1939c1001ee
Wes Bubeck <wesbubeck20@gmail.com>
2017-08-08 23:33:08 -0400

    Psuedo coding breed array

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
 
 //Array that will hold youtube iframe with breed vids index will match breed value in dropdown
 var breedVids = ['<iframe width="360" height="203" src="https://www.youtube.com/embed/BeSHBdmlv1g" frameborder="0" allowfullscreen></iframe>']
+
+//Create an empty array that will hold dog breed that are returned from an API call
+var data = []
