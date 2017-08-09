c0865159592e53f7d493e9864ee4a912614f23f9
Wes Bubeck <wesbubeck20@gmail.com>
2017-08-08 23:16:07 -0400

    vid array

---
+++ assets/js/app.js
@@ -0,0 +1,3 @@
+//This Var will contain vids that will match breed index
+
+var breedVids = ['<iframe width="360" height="203" src="https://www.youtube.com/embed/BeSHBdmlv1g" frameborder="0" allowfullscreen></iframe>']
