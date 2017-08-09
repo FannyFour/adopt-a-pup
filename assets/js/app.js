ffb1bf91df1672b4092c84c953eafef843ea71eb
Wes Bubeck <wesbubeck20@gmail.com>
2017-08-08 23:31:21 -0400

    vid array started

---
+++ assets/js/app.js
@@ -0,0 +1,3 @@
+
+//Array that will hold youtube iframe with breed vids index will match breed value in dropdown
+var breedVids = ['<iframe width="360" height="203" src="https://www.youtube.com/embed/BeSHBdmlv1g" frameborder="0" allowfullscreen></iframe>']
