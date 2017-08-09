c0c0e2958b020cf7415ee54a1c908bc2252c6090
Wes Bubeck <wesbubeck20@gmail.com>
2017-08-09 00:19:18 -0400

    adding vids

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 
 //Array that will hold youtube iframe with breed vids index will match breed value in dropdown
-var breedVids = ['<iframe width="360" height="203" src="https://www.youtube.com/embed/BeSHBdmlv1g" frameborder="0" allowfullscreen></iframe>']
+var breedVids = ['<iframe width="360" height="203" src="https://www.youtube.com/embed/ohnkNMFSnAc" frameborder="0" allowfullscreen></iframe>',
+'<iframe width="360" height="203" src="https://www.youtube.com/embed/d3IwYck7zgo" frameborder="0" allowfullscreen></iframe>',
+'<iframe width="360" height="203" src="https://www.youtube.com/embed/1yWd6_gF5OQ" frameborder="0" allowfullscreen></iframe>',
+'<iframe width="360" height="203" src="https://www.youtube.com/embed/BeSHBdmlv1g" frameborder="0" allowfullscreen></iframe>']
 
 //Create an empty array that will hold dog breed that are returned from an API call
 var data = []
